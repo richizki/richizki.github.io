d32dd68aa840cc472ce9c1bd56d8bb5fe7e3df19
Rizki Fitriani <47499259+richizki@users.noreply.github.com>
2025-10-07 17:49:21 +0700

    Delete Reactfolio-main directory

--- Reactfolio-main/src/components/components.js
+++
@@ -1,7 +0,0 @@
-import Footer from "./Footer/Footer";
-import Navbar from "./Navbar/Navbar";
-import Project from "./Project/Project";
-import PageTitle from "./PageTitle/PageTitle";
-import Certificate from "./Certificate/Certificate";
-
-export { Footer, Navbar, Project, PageTitle, Certificate };
--- Reactfolio-main/vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/your-repo-name/", // This will be dynamically updated by GitHub Actions
-});
